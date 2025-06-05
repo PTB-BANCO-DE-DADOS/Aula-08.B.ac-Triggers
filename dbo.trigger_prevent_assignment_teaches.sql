499321b765f33b775fb5eca566cb76d2e6f529e4
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-06-04 23:36:34 -0300

    Update dbo.trigger_prevent_assignment_teaches.sql

--- dbo.trigger_prevent_assignment_teaches.sql
+++ dbo.trigger_prevent_assignment_teaches.sql
@@ -1,28 +1,26 @@
 --Questão 01. Crie uma Trigger denominada dbo.trigger_prevent_assignment_teaches para impedir que aulas sejam atribuidas a um instrutor que já possui 2 ou mais atribuições no ano.
-CREATE TRIGGER dbo.trigger_prevent_assignment_teaches
-ON dbo.teaches
+CREATE OR ALTER TRIGGER dbo.trigger_prevent_assignment_teaches
+ON teaches
 INSTEAD OF INSERT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Verificar cada linha que está sendo inserida
     IF EXISTS (
-        SELECT i.ID, i.year
+        SELECT 1
         FROM inserted i
-        JOIN teaches t
-            ON i.ID = t.ID AND i.year = t.year
-        GROUP BY i.ID, i.year
-        HAVING COUNT(t.course_id) + COUNT(i.course_id) > 2
+        JOIN (
+            SELECT ID, year, COUNT(*) AS qtd
+            FROM teaches
+            GROUP BY ID, year
+        ) t ON i.ID = t.ID AND i.year = t.year
+        WHERE t.qtd >= 2
     )
     BEGIN
-        -- Gerar erro e impedir inserção
-        RAISERROR ('Este instrutor já possui 2 ou mais atribuições neste ano.', 16, 1);
-        ROLLBACK TRANSACTION;
+        RAISERROR(N'Este instrutor já possui 2 ou mais aulas atribuídas neste ano.', 16, 1);
         RETURN;
-    END
+    END;
 
-    -- Se não violar, então faz o insert normalmente
     INSERT INTO teaches (ID, course_id, sec_id, semester, year)
     SELECT ID, course_id, sec_id, semester, year
     FROM inserted;
